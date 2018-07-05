94e0a165f7989fff5b5d9d4ff124083c57a9d233
Michael Morris <mdam2345@gmail.com>
2018-07-05 00:51:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
 
   def match(array)
     array.select do |element|
-      (@word.split("").sort) == (element.split("").sort)
+      @word.split("").sort) == (element.split("").sort
    end
  end
 end
